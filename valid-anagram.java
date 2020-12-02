b61e7f458786f3e120d30ba4cf4ed31a6663b48d
Na Nazhou <nazhou.na9@gmail.com>
2020-12-02 16:43:16 +0800

    working commit - Created using LeetHub

---
+++ valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch: s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        for (char ch: t.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+            
+            if (map.get(ch) == 1) {
+                map.remove(ch);
+            } else {
+                map.put(ch, map.get(ch) - 1);
+            }
+        }
+        
+        return map.isEmpty();
+    }
+}
