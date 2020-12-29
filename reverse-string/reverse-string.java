46f78632ab94cbbb8a329a2f0c1e9e6d5e285d5a
Na Nazhou <nazhou.na9@gmail.com>
2020-12-30 02:10:12 +0800

    Time: 1 ms (95.50%), Space: 45.6 MB (89.18%) - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,13 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-        while (left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        int start = 0, end = s.length - 1;
+        while (start < end) {
+            char temp = s[start];
+            s[start++] = s[end];
+            s[end--] = temp;
         }
     }
 }
