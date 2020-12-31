b09a06368327401b04f8a896eee3dcd570d7c711
Na Nazhou <nazhou.na9@gmail.com>
2020-12-31 19:48:04 +0800

    Time: 4 ms (10.48%), Space: 38.8 MB (97.33%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -2,26 +2,27 @@ class Solution {
     private List<List<Integer>> perms = new ArrayList<>();
     
     public List<List<Integer>> permute(int[] nums) {
-        permute(nums, 0);
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        permute(nums.length, set, new ArrayList<>());
         return perms;
     }
     
-    private void permute(int[] curr, int i) {
-        if (i == curr.length) {
-            perms.add(Arrays.stream(curr).boxed().collect(Collectors.toList()));
+    private void permute(int n, Set<Integer> nums, List<Integer> curr) {
+        if (curr.size() == n) {
+            perms.add(new ArrayList<Integer>(curr));
             return;
         }
         
-        for (int k = i; k < curr.length; k++) {
-            swap(curr, i, k);
-            permute(curr, i + 1);
-            swap(curr, i, k);
+        ArrayList<Integer> arr = new ArrayList<>(nums);
+        for (int num : arr) {
+            nums.remove(num);
+            curr.add(num);
+            permute(n, nums, curr);
+            nums.add(num);
+            curr.remove(curr.size() - 1);
         }    
-    }
-    
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
