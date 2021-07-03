4b1b9acaad7ee690e7d49a241dc76687e828e1b8
Na Nazhou <nazhou.na9@gmail.com>
2021-07-03 16:43:00 +0800

    Time: 1 ms (86.31%), Space: 38.9 MB (89.76%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (map.containsKey(num)) {
-                return new int[]{ map.get(num), i};
-            }
-            
-            map.put(target - num, i);
-        }
-        
-        return new int[]{};
-    }
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        // key: number; value: index
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(target - num)) {
+                return new int[]{map.get(target - num), i};
+            } else {
+                map.put(num, i);
+            }
+        }
+        
+        throw new IllegalArgumentException("Solution not found.");
+    }
 }
