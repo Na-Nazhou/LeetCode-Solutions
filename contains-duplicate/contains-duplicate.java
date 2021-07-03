f26776bae52b4b22bd99bd37eaa2611d33372ce5
Na Nazhou <nazhou.na9@gmail.com>
2021-07-03 17:22:34 +0800

    Time: 6 ms (55.37%), Space: 44.9 MB (49.97%) - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,14 +1,14 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true;
-            }
-            
-            set.add(num);
-        }
-        
-        return false;
-    }
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            
+            set.add(num);
+        }
+        
+        return false;
+    }
 }
