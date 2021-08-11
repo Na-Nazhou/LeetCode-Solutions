a47bd38a0f53e60b12fbcd374f47373cd76ce721
Na Nazhou <nazhou.na9@gmail.com>
2021-08-11 17:55:21 +0800

    Time: 1 ms (100.00%), Space: 36.1 MB (55.00%) - LeetHub

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -1,15 +1,27 @@
 class Solution {
-    public int reverse(int x) {
-        long answer = 0;
-        while (x != 0) {
-            int lsd = x % 10;
-            x /= 10;
-            answer = answer * 10 + lsd;
-        }
-        if (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
-            return 0;
-        } else {
-            return (int) answer;
-        }
-    }
+    public int reverse(int x) {
+        int val = Math.abs(x);
+        int curr = 0;
+        boolean isPositive = x >= 0;
+        
+        while (val > 0) {
+            int digit = val % 10;
+            
+            if (isPositive) {
+                if (curr > (Integer.MAX_VALUE - digit) / 10) {
+                    return 0;
+                }
+                curr = curr * 10 + digit;
+            } else {
+                if (curr < (Integer.MIN_VALUE + digit) / 10) {
+                    return 0;
+                }
+                curr = curr * 10 - digit;
+            }
+            
+            val = val / 10;
+        }
+        
+        return curr;
+    }
 }
