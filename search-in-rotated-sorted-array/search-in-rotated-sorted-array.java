1e15e7d304165bb5e25b24d232c7e9f646f402c6
Na Nazhou <nazhou.na9@gmail.com>
2021-08-27 17:52:25 +0800

    Time: 1 ms (14.82%), Space: 39.4 MB (6.07%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,35 +1,40 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
-        int left = 0;
-        int right = n - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < nums[right]) {
-                right = mid;
-            } else {
-                left = mid + 1; 
-            }
-        }
-        
-        int start = left;
-        left = 0;
-        right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midVal = nums[(mid + start) % n];
-            if (midVal == target) {
-                return (mid + start) % n;
-            }
-            
-            if (midVal > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-            
-        }
-        
-        return -1;
-    }
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        boolean isOnLeft = target >= nums[0];
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            }
+            
+            if (isOnLeft) {
+                if (num < target) {
+                    if (num >= nums[0]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (num < target) {
+                    left = mid + 1;
+                } else {
+                    if (num >= nums[0]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
 }
