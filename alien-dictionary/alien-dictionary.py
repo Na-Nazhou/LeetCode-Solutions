8bf7d49f18f1a532d973ecf8f94da7d5a13d9110
Na Nazhou <nazhou.na9@gmail.com>
2021-09-18 12:33:51 +0800

    Time: 54 ms (12.17%), Space: 14.5 MB (25.33%) - LeetHub

--- alien-dictionary/alien-dictionary.py
+++ alien-dictionary/alien-dictionary.py
@@ -1,38 +1,69 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj_list = defaultdict(set)
-        in_degree = {}
-        for word in words:
-            for c in word:
-                in_degree[c] = 0
+        g = {}
+        in_degree = defaultdict(int)
+        
+        # IMPORTANT!!!
+        if len(words) == 1:
+            return "".join(list(set(list(words[0]))))
         
         for i in range(len(words) - 1):
-            first = words[i]
-            second = words[i + 1]
-            
+            word1 = words[i]
+            word2 = words[i + 1]
             foundDiff = False
-            for c1, c2 in zip(first, second):
-                if c1 != c2:
-                    foundDiff = True
-                    if c2 not in adj_list[c1]:
-                        adj_list[c1].add(c2)
-                        in_degree[c2] += 1
-                    break
+            ptr1 = 0
+            ptr2 = 0
+            while ptr1 < len(word1) and ptr2 < len(word2):
+                c1 = word1[ptr1]
+                c2 = word2[ptr2]
+                ptr1 += 1
+                ptr2 += 1
+                
+                if c1 not in g:
+                    g[c1] = []
+                if c2 not in g:
+                    g[c2] = []
                     
-            if len(first) > len(second) and not foundDiff:
+                if c1 == c2:
+                    continue
+                
+                foundDiff = True
+                g[c1].append(c2)
+                in_degree[c2] += 1
+                break
+            
+            if not foundDiff and len(word1) > len(word2):
                 return ""
             
-        output = []
-        q = deque([c for c in in_degree if in_degree[c] == 0])
-        while q:
-            c1 = q.popleft()
-            output.append(c1)
-            for c2 in adj_list[c1]:
-                in_degree[c2] -= 1
-                if in_degree[c2] == 0:
-                    q.append(c2)
+            while ptr1 < len(word1):
+                c = word1[ptr1]
+                if c not in g:
+                    g[c] = []
+                ptr1 += 1
+                print(c)
+            
+            while ptr2 < len(word2):
+                c = word2[ptr2]
+                if c not in g:
+                    g[c] = []
+                ptr2 += 1
+                print(c)
+
+        q = deque()
+        res = []
+        for v in g.keys():
+            if in_degree[v] == 0:
+                q.append(v)
         
-        if len(output) < len(in_degree):
+        while q:
+            v = q.popleft()
+            res.append(v)
+            for neighbor in g[v]:                    
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    q.append(neighbor)
+                
+        if len(res) != len(g.keys()):
             return ""
         
-        return "".join(output)
+        return "".join(res)
