320ea2e010bf9defd3466a4ef857b9e5c2677a1e
Na Nazhou <nazhou.na9@gmail.com>
2021-09-23 14:22:57 +0800

    Time: 320 ms (5.02%), Space: 15.2 MB (99.78%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.py
+++ basic-calculator-ii/basic-calculator-ii.py
@@ -1,22 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        num, stack, sign = 0, [], "+"
-        for i in range(len(s)):
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            if s[i] in "+-*/" or i == len(s) - 1:
-                if sign == "+":
-                    stack.append(num)
-                elif sign == "-":
-                    stack.append(-num)
-                elif sign == "*":
-                    stack.append(stack.pop()*num)
-                else:
-                    stack.append(int(stack.pop()/num))
-                num = 0
-                sign = s[i]
-        return sum(stack)
-                            
-                        
-                    
-                
+        res = 0
+        
+        prev = 0
+        op = '+'
+        curr = 0
+        
+        # Imagine we add a "0+" at the front of the expression
+        
+        for i, c in enumerate(s):
+            if c.isdigit():
+                curr = curr * 10 + int(c)
+            
+            if not c.isdigit() and not c.isspace() or i == len(s) - 1:
+                if op == "+":
+                    res += prev
+                    prev = curr
+                if op == "-":
+                    res += prev
+                    prev = -curr
+                if op == "*":
+                    prev *= curr
+                if op == "/":
+                    prev = int(prev / curr)
+                op = c
+                curr = 0
+            
+            print(c, res, prev, op, curr)
+        
+        res += prev
+        print(prev, op, curr)
+        return res
