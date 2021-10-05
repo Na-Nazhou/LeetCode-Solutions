792888c9d685fe14f5490412f0b743aa51acb833
Na Nazhou <nazhou.na9@gmail.com>
2021-10-05 19:44:28 +0800

    Time: 166 ms (34.20%), Space: 17.8 MB (5.09%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,38 +1,37 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         bal = 0
-        ans = []
-        
-        # Remove extra closing brackets
+        s_without_extra_close_bracket = []
         for c in s:
-            if c == ')' and bal == 0:
-                continue
-            
             if c == '(':
                 bal += 1
+            elif c== ')':
+                if bal == 0:
+                    continue
                 
-            if c == ')':
                 bal -= 1
             
-            ans.append(c)
+            s_without_extra_close_bracket.append(c)
         
-        s = "".join(ans)
+        if bal == 0:
+            return "".join(s_without_extra_close_bracket)
+        
         bal = 0
-        ans = []
-        
-        # Remove extra open brackets
-        for i in range(len(s) - 1, -1, -1):
-            c = s[i]
-            if c == '(' and bal == 0:
-                continue
-            
-            if c == ')':
+        s_without_extra_open_backet = []
+        for i in range(len(s_without_extra_close_bracket) - 1, -1, -1):
+            c = s_without_extra_close_bracket[i]
+            if c == '(':
+                if bal == 0:
+                    continue
+                    
+                bal -= 1
+            elif c== ')':
                 bal += 1
             
-            if c == '(':
-                bal -= 1
+            s_without_extra_open_backet.append(c)
+        
+        return "".join(s_without_extra_open_backet[::-1])
+
             
-            ans.append(c)
                 
-        ans.reverse()
-        return "".join(ans)
+                    
