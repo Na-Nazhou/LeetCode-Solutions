6fd06219d36e515638dffa5ffc9f4aa7009dcb69
Na Nazhou <nazhou.na9@gmail.com>
2021-10-05 19:57:14 +0800

    Time: 118 ms (70.47%), Space: 14.7 MB (17.26%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.py
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,17 +1,18 @@
 class Solution:
-    
     def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(s):
+            return s == s[::-1]
         
-        def is_palindrome(s):
-            return s[::-1] == s
-    
-        i, j = 0, len(s) - 1
-        while i < j:
-            if s[i] == s[j]:
-                i += 1
-                j -= 1
-                continue
-            
-            return is_palindrome(s[i+1:j+1]) or is_palindrome(s[i:j])
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            c1 = s[start]
+            c2 = s[end]
+            if c1 != c2:
+                return isPalindrome(s[start:end]) or isPalindrome(s[start + 1:end + 1])
+            start += 1
+            end -= 1
         
         return True
+        
+        
