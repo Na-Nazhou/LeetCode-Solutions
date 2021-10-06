47ea21f65f9a10357d51750cc1552c3a3423d386
Na Nazhou <nazhou.na9@gmail.com>
2021-10-06 21:19:12 +0800

    Time: 3560 ms (71.31%), Space: 40 MB (28.57%) - LeetHub

--- making-a-large-island/making-a-large-island.py
+++ making-a-large-island/making-a-large-island.py
@@ -1,51 +1,70 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        component_size = defaultdict(int) # key: component_id, size of the component
         
-        num_of_islands = 0
-        size_map = defaultdict(int) # key: label, value: size
-        island_map = {} # key: pos, value: label
         m = len(grid)
         n = len(grid[0])
         
-        def within_bound(i, j):
-            return i >= 0 and i < m and j >= 0 and j < n
+        visited = [ [False] * n for _ in range(m) ]
+        components = [ [None] * n for _ in range(m) ]
         
-        def dfs(i, j, label, visited):
+        def dfs(i, j, component_id):
             visited[i][j] = True
-            size_map[label] += 1
-            island_map[(i, j)] = label
             
-            for di, dj in dirs:
+            component_size[component_id] += 1
+            components[i][j] = component_id
+            
+            dircs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for di, dj in dircs:
                 new_i = i + di
                 new_j = j + dj
-                if within_bound(new_i, new_j) and grid[new_i][new_j] == 1 and not visited[new_i][new_j]:
-                    dfs(new_i, new_j, label, visited)
+                
+                if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n:
+                    continue
+                
+                if visited[new_i][new_j]:
+                    continue
+                
+                if grid[new_i][new_j] != 1:
+                    continue
+                    
+                dfs(new_i, new_j, component_id)
+            
         
-        visited = [[False] * n for _ in range(m)]
+        curr_component_id = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1 and not visited[i][j]:
-                    dfs(i, j, num_of_islands, visited)
-                    num_of_islands += 1
+                    dfs(i, j, curr_component_id)
+                    curr_component_id += 1
         
         max_area = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    area = 1
-                    labels = set()
-                    for di, dj in dirs:
+                    size = 1
+                    dircs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                    components_set = set()
+                    for di, dj in dircs:
                         new_i = i + di
                         new_j = j + dj
-                        if (new_i, new_j) in island_map:
-                            label = island_map[(new_i, new_j)]
-                            if label not in labels:
-                                area += size_map[label]
-                                labels.add(label)
-                    max_area = max(max_area, area)
+
+                        if new_i < 0 or new_i >= m or new_j < 0 or new_j >= n:
+                            continue
+                        
+                        if grid[new_i][new_j] != 1:
+                            continue
+                        
+                        component_id = components[new_i][new_j]
+                        if component_id not in components_set:
+                            components_set.add(component_id)
+                            size += component_size[component_id]
+                        
+                    max_area = max(max_area, size)
+                else:
+                    component_id = components[i][j]
+                    size = component_size[component_id]
+                    max_area = max(max_area, size)
         
-        if max_area == 0:
-            return max(size_map.values())
-        else:
-            return max_area
+        return max_area
+                        
