8bd256eb12e839c14eac333feb30c5c179323e12
Na Nazhou <nazhou.na9@gmail.com>
2021-10-06 20:42:40 +0800

    Time: 676 ms (73.97%), Space: 20 MB (54.02%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.py
+++ k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,37 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        h = [] # max heap
         
-        def get_distance(pt):
-            return sqrt(pt[0] ** 2 + pt[1] ** 2)
-    
-        def quick_select(i, j, k):
-            if i == j:
-                return
-            pivot_idx = partition(i, j)
-            if pivot_idx == k:
-                return
-            
-            if pivot_idx < k:
-                return quick_select(pivot_idx + 1, j, k)
-            else:
-                return quick_select(i, pivot_idx - 1, k)
+        def distance(p):
+            return sqrt(p[0] ** 2 + p[1] ** 2)
         
-        def partition(i, j):
-            pivot_idx = random.randint(i, j)
-            swap(i, pivot_idx)
-            ptr = i # last index <= pivot
-            pivot = get_distance(points[i])
-            for idx in range(i + 1, j + 1):
-                if get_distance(points[idx]) <= pivot:
-                    ptr += 1
-                    swap(idx, ptr)
-            swap(i, ptr)
-            
-            return ptr
-                    
+        for point in points:
+            dist = distance(point)
+            heapq.heappush(h, (-dist, point))
+            if len(h) > k:
+                heapq.heappop(h)
         
-        def swap(i, j):
-            points[i], points[j] = points[j], points[i]
+        ans = [pair[1] for pair in h]
+        return ans
             
-        quick_select(0, len(points) - 1, k - 1)
-        return points[:k]
+        
