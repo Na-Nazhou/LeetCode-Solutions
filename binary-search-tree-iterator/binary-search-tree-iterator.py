b1bc9293a244841a49c9b56f50545983809a0b0e
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 17:05:33 +0800

    Time: 76 ms (65.60%), Space: 20.7 MB (19.30%) - LeetHub

--- binary-search-tree-iterator/binary-search-tree-iterator.py
+++ binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -5,27 +5,25 @@
 #         self.left = left
 #         self.right = right
 class BSTIterator:
-    
-    def inorder(self, root, res):
-        if root is None:
-            return
-        
-        self.inorder(root.left, res)
-        res.append(root.val)
-        self.inorder(root.right, res)
 
     def __init__(self, root: Optional[TreeNode]):
-        self.res = []
-        self.inorder(root, self.res)
-        self.ptr = 0
+        self.stack = []
+        self.populate_left_subtree(root)
+    
+    def populate_left_subtree(self, root):
+        curr = root
+        while curr:
+            self.stack.append(curr)
+            curr = curr.left
 
     def next(self) -> int:
-        val = self.res[self.ptr]
-        self.ptr += 1
-        return val
+        node = self.stack.pop()
+        self.populate_left_subtree(node.right)
+        
+        return node.val
 
     def hasNext(self) -> bool:
-        return self.ptr < len(self.res)
+        return len(self.stack) != 0
 
 
 # Your BSTIterator object will be instantiated and called as such:
