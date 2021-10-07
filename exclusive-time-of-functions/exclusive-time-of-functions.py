96c6899b01a73231c8c232fe63581b938eba2a1f
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 11:16:15 +0800

    Time: 76 ms (56.65%), Space: 14.4 MB (55.39%) - LeetHub

--- exclusive-time-of-functions/exclusive-time-of-functions.py
+++ exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,28 +1,30 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        
         def parse_log(log):
             params = log.split(":")
-            f_id = int(params[0])
+            func_id = int(params[0])
             event = params[1]
             ts = int(params[2])
-            return f_id, event, ts
-        
-        res = [0] * n
-        stack = []
-        prev_ts = None
-        for log in logs:
-            f_id, event, ts = parse_log(log)
             
+            return (func_id, event, ts)
+        
+        ans = [0] * n
+        
+        prev_ts = 0
+        stack = []
+        for log in logs:
+            func_id, event, ts = parse_log(log)
             if event == "start":
                 if stack:
-                    prev_f = stack[-1]
-                    res[prev_f] += ts - prev_ts
-                stack.append(f_id)
+                    prev_func_id = stack[-1]
+                    ans[prev_func_id] += ts - prev_ts
+                stack.append(func_id)
                 prev_ts = ts
+            
             if event == "end":
-                prev_f = stack.pop()
-                res[prev_f] += ts - prev_ts + 1
+                prev_func_id = stack.pop() # assert prev_func_id == func_id
+                ans[prev_func_id] += ts + 1 - prev_ts
                 prev_ts = ts + 1
         
-        return res
-                
+        return ans
