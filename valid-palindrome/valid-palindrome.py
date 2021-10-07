1d52c3b3a284fa59dc267eadaafa1ab78ea38246
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 22:10:38 +0800

    Time: 44 ms (81.72%), Space: 14.6 MB (61.52%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,21 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        front = 0
-        back = len(s) - 1
-        s = s.lower()
-        while front < back:
-            if not s[front].isalnum():
-                front += 1
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
                 continue
             
-            if not s[back].isalnum():
-                back -= 1
+            if not s[end].isalnum():
+                end -= 1
                 continue
             
-            if s[front] != s[back]:
+            if s[start].lower() != s[end].lower():
                 return False
             
-            front += 1
-            back -= 1
+            start += 1
+            end -= 1
         
         return True
