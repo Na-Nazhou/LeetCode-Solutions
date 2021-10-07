1d3ca45122a0175248ca296df30abfca2c3bc626
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 11:58:22 +0800

    Time: 24 ms (96.08%), Space: 14 MB (98.86%) - LeetHub

--- maximum-swap/maximum-swap.py
+++ maximum-swap/maximum-swap.py
@@ -1,32 +1,22 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        digits = [ int(c) for c in str(num)]
         
-        # First a such that digits[a] < digits[a + 1]
-        a = None
-        for i in range(len(digits) - 1):
-            curr = digits[i]
-            next = digits[i + 1]
-            if next > curr:
-                a = i
-                break
+        num = list(str(num))
+        curr_max = -1
+        curr_max_idx = None
+        swap = None
         
-        if a is None:
-            return num
+        for i in range(len(num) - 1, -1, -1):
+            digit = int(num[i])
+            if digit > curr_max:
+                curr_max = digit
+                curr_max_idx = i
+            elif digit < curr_max:
+                swap = [i, curr_max_idx]
         
-        # Last b which is the maximum in digits[a+1:]
-        b = a + 1
-        for i in range(a + 2, len(digits)):
-            if digits[i] >= digits[b]:
-                b = i
-        for i in range(a + 1):
-            if digits[i] < digits[b]:
-                digits[i], digits[b] = digits[b], digits[i]
-                break
+        if swap is None:
+            return int("".join(num))
         
-        res = 0
-        for digit in digits:
-            res = res * 10 + digit
+        num[swap[0]], num[swap[1]] = num[swap[1]], num[swap[0]]
         
-        return res
-        
+        return int("".join(num))
