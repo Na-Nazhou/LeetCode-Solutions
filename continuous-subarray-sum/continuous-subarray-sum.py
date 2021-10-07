3a53d0adc14bf1855e1cb7fa0fa860359c0053dc
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 11:41:37 +0800

    Time: 924 ms (47.90%), Space: 33.9 MB (19.41%) - LeetHub

--- continuous-subarray-sum/continuous-subarray-sum.py
+++ continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        s = {}
-        curr = 0
-        s[curr] = -1
+        prefix_modulo = {}
+        prefix_modulo[0] = -1
+        curr_modulo = 0
         for i, num in enumerate(nums):
-            curr = (curr + num) % k
-            if curr in s:
-                if s[curr] != i - 1:
-                    return True
-            else:
-                s[curr] = i
+            curr_modulo = (curr_modulo + num) % k
+            if curr_modulo in prefix_modulo and prefix_modulo[curr_modulo] < i - 1:
+                return True
+            if curr_modulo not in prefix_modulo:
+                prefix_modulo[curr_modulo] = i
+        
         return False
