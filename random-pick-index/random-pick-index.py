d20f8545079f5712cb44ce93a823f588a8772083
Na Nazhou <nazhou.na9@gmail.com>
2021-10-07 15:49:56 +0800

    Time: 328 ms (44.54%), Space: 23.7 MB (26.11%) - LeetHub

--- random-pick-index/random-pick-index.py
+++ random-pick-index/random-pick-index.py
@@ -1,14 +1,20 @@
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.m = defaultdict(list)
+        self.map = {}
         for i, num in enumerate(nums):
-            self.m[num].append(i)
+            if num not in self.map:
+                self.map[num] = []
+            self.map[num].append(i)
+            
 
     def pick(self, target: int) -> int:
-        choices = self.m[target]
-        idx = random.randint(0, len(choices) - 1)
-        return choices[idx]
+        if target not in self.map:
+            raise ValueError()
+        
+        options = self.map[target]
+        idx = random.randint(0, len(options) -1)
+        return options[idx]
 
 
 # Your Solution object will be instantiated and called as such:
