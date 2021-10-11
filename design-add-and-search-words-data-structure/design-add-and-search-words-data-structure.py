4a6860376f9c38e357e5398f2cb34a8f26e1e68e
Na Nazhou <nazhou.na9@gmail.com>
2021-10-11 15:48:32 +0800

    Time: 436 ms (22.88%), Space: 31 MB (6.56%) - LeetHub

--- design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,17 +1,13 @@
 class TrieNode:
     
     def __init__(self):
-        self.children = [None]* 26
-        self.eow = False
+        self.children = [None] * 26
+        self.is_word = False
         
 class WordDictionary:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
-        
 
     def addWord(self, word: str) -> None:
         curr = self.root
@@ -21,25 +17,27 @@ class WordDictionary:
                 curr.children[idx] = TrieNode()
             curr = curr.children[idx]
         
-        curr.eow = True
+        curr.is_word = True
 
     def search(self, word: str) -> bool:
-        def helper(node, i) -> bool:
-            if node is None:
+        
+        def helper(curr, i):
+            if curr is None:
                 return False
-
+            
             if i == len(word):
-                return node.eow
-
-            if word[i] == ".":
-                for child in node.children:
-                    if helper(child, i+1):
+                return curr.is_word
+            
+            c = word[i]
+            if c == '.':
+                for child in curr.children:
+                    if helper(child, i + 1):
                         return True
                 return False
             else:
-                idx = ord(word[i]) - ord('a')
-                return helper(node.children[idx], i+1)
-            
+                idx = ord(c) - ord('a')
+                return helper(curr.children[idx], i + 1)
+        
         return helper(self.root, 0)
 
 
