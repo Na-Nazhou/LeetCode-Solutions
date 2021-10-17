1f7aa53e28505f569d3108974f37aad45579e08e
Na Nazhou <nazhou.na9@gmail.com>
2021-10-17 12:47:54 +0800

    Time: 56 ms (19.29%), Space: 15.5 MB (26.10%) - LeetHub

--- convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -9,40 +9,35 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        def dfs(root):
-            if root.left is None and root.right is None:
-                return (root, root)
-            if root.left is None:
-                right_start, right_end = dfs(root.right)
-                root.right = right_start
-                right_start.left = root
-                return (root, right_end)
-            if root.right is None:
-                left_start, left_end = dfs(root.left)
-                root.left = left_end
-                left_end.right = root
-                return (left_start, root)
-
-            left_start, left_end = dfs(root.left)
-            right_start, right_end = dfs(root.right)
-            root.left = left_end
-            left_end.right = root
-            root.right = right_start
-            right_start.left = root
-            return left_start, right_end
-
+        
+        def helper(root):
+            ans_left = root
+            ans_right = root
+            
+            if root.left is not None:
+                left, right = helper(root.left)
+                
+                root.left = right
+                right.right = root
+                
+                ans_left = left
+            
+            if root.right is not None:
+                left, right = helper(root.right)
+                
+                root.right = left
+                left.left = root
+                
+                ans_right = right
+            
+            return ans_left, ans_right
+        
         if root is None:
             return None
-
-        start, end = dfs(root)
-        start.left = end
-        end.right = start
         
-        # curr = start
-        # while True:
-        #     print(curr.val)
-        #     curr = curr.right
-        #     if curr == start:
-        #         break
-
-        return start
+        left, right = helper(root)
+        
+        left.left = right
+        right.right = left
+        
+        return left
