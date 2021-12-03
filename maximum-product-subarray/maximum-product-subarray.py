91e748eb439faf13ceb51615a6ef0579b4cff6ee
Na Nazhou <nazhou.na9@gmail.com>
2021-12-03 11:51:46 +0800

    Time: 60 ms (43.10%), Space: 14.4 MB (63.75%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -6,9 +6,9 @@ class Solution:
         
         for i in range(1, len(nums)):
             num = nums[i]
-            temp = max_product
-            max_product = max(max_product * num, min_product * num, num)
-            min_product = min(temp * num, min_product * num, num)
+            candidates = [max_product * num, min_product * num, num]
+            max_product = max(candidates)
+            min_product = min(candidates)
             ans = max(ans, max_product, min_product)
         
         return ans
